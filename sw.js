20e7705463955e0145c78f0f755f4f3e752e1c79
اسم المستخدم الخاص بك < >
2026-03-16 12:59:08 +0300

    ✨ تحديث Service Worker للتخزين المؤقت

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'speedfusion-v1';
+const CACHE_NAME = 'speedfusion-v2';
 const urlsToCache = [
   '/speedfusion-pwa/',
   '/speedfusion-pwa/index.html',
-  '/speedfusion-pwa/manifest.json'
+  '/speedfusion-pwa/manifest.json',
+  '/speedfusion-pwa/icons/icon-192x192.png',
+  '/speedfusion-pwa/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -14,7 +16,18 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      if (event.request.mode === 'navigate') {
+        return caches.match('/speedfusion-pwa/index.html');
+      }
+    })
   );
 });
 
